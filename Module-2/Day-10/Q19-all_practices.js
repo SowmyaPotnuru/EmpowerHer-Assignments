460f77a374be3e2f417318bd7befc4797dd83b38
SowmyaPotnuru <sowmyapotnuru781@gmail.com>
2025-11-24 22:04:32 +0530

    Adding Question 19 to the Git

--- Module-2/Day-10/Q19-all_practices.js
+++ Module-2/Day-10/Q19-all_practices.js
@@ -93,10 +93,173 @@ console.log(i);
 // last iteration value and prints it.
 
 // b) 
-for (let j = 0; j < 3; j++) {}
-console.log(j);
+// for (let j = 0; j < 3; j++) {}
+// console.log(j);
 // There is an error, ReferenceError: j is not defined because j uses 'let' keyword here,
 // and it belongs to the block scope.
 
 // c) Why is const used for values that should not be reassigned?
-// Because 'const' is block-scope, so it belongs to the particular value and it cannot be reassigned.
+// Because 'const' is block-scope, so this tells the value belongs to the particular variable only and it cannot be reassigned/changed.
+
+console.log("-------------")
+
+// 6. Ternary Operator – Practice
+// a)
+let speed = kmph => (kmph > 60) ? "Fast" : "Normal"
+console.log(speed(78));
+
+// b)
+function findAge(age){
+  return (age>=18) ? "Adult" : "Minor"
+}
+console.log(findAge(32))
+
+// c)
+const checkNum = num => {
+  return (num>0) ? "Positive" :
+         (num==0) ? "Zero" :
+        "Negative"
+}
+console.log(checkNum(15));
+
+console.log("-------------")
+
+// 7. Spread, Rest & Arrays
+// a) Add elements 4, 5 to this array using spread:
+const nums = [1,2,3];
+console.log([...nums,4,5]);
+
+// b) Combine these arrays using spread:
+let a5 = ["x","y"];
+let b5 = ["z"];
+const c = [...a5, ...b5]
+console.log(c);
+
+// c)Write a function using rest: printNames("A","B","C") → returns ["A","B","C"].
+function printNames(...rest){
+  return rest;
+}
+console.log(printNames("A","B","C"));
+
+console.log("-------------")
+
+// 8. Object Destructuring & Shorthand
+// a) Destructure the following:
+const user = { id: 101, status: "active" };
+const {id, status} = user;
+console.log(id, status);
+
+// b) Convert this to shorthand:
+const newId = 101;
+const role = "admin";
+const newUser = {
+  newId,
+  role
+};
+console.log(newUser);
+
+// c) Create an object using shorthand and add a method using shorthand syntax.
+const item = "pen";
+const quantity = 2;
+const price = 10;
+const itemList = {
+  item,
+  quantity,
+  price,
+  totalAmt(){
+    console.log(`Total Amount: ${this.quantity*this.price}`);
+  }
+}
+itemList.totalAmt();
+
+console.log("-------------")
+
+// 9. Template Literals (More Practice)
+// a) Use a template literal to print today’s date using: new Date().toDateString() 
+console.log(`Today's Date is: ${new Date().toDateString() }`);
+
+// b) Create a template literal that prints: "Hello NAME, your score is SCORE/100"
+const studName = "Akhil";
+const studScore = 85;
+console.log(`Hello ${studName}, your score is ${studScore}/100.`);
+
+console.log("-------------")
+
+// 10. Arrow Function Shorthands
+// a) Convert a normal function to a one-line arrow function for addition.
+const addition = (a,b) => a+b;
+console.log("Addition of 3 and 5 is:",addition(3,5));
+
+// b) Write an arrow function isAdult(age) that returns true/false.
+const isAdult = age => {
+  if(age>=18){
+    return true
+  }else{
+    return false
+  }
+}
+console.log("Greater than age 18:",isAdult(25));
+
+// c) Create an arrow function double that doubles a number.
+const double = num => num*num 
+console.log("Double the number:",double(9));
+
+console.log("-------------")
+
+// 11. Spread Operator (Arrays & Objects)
+// a) Clone an array using spread.
+let alpArr = ["S","O","W","M","Y","A"]
+console.log(...alpArr);
+
+// b) Add element 100 to the beginning of an array using spread.
+let marks = [80,85,70,67];
+let newMarks = [100,...marks];
+console.log(newMarks);
+
+// c) Merge two objects and override one property using spread.
+let schl = {
+  name: "Tiny Tots",
+  noOfStudents: 75,
+  appraisals: 5,
+  address: {
+    city: "Visakhapatnam",
+    zipcode: 530013
+  }
+}
+let studList = {
+  noOfStudents : 150,
+  totalBoys: 80,
+  totalGirls: 70
+}
+let schlObj = {...schl, ...studList};
+console.log(schlObj);
+
+console.log("-------------")
+
+// 12. Optional Chaining (More Practice)
+// a) Access user.address.city safely:
+const userDetails = {
+  name: "Alex",
+  address: {
+    city: "Bangalore"
+  }
+};
+console.log(userDetails.address?.city);
+
+// b) Access user.job.title safely (should print undefined).
+console.log(userDetails.address?.title);
+
+// c)Write an example where optional chaining prevents a runtime error.
+const userA = {
+  name: "Alice",
+  address: {
+    city: "Mumbai",
+    // note: address.zipcode is missing
+  }
+};
+const zipcodeLength = userA.address.zipcode?.length;
+console.log(zipcodeLength);  // undefined (no error)
+
+// Here in userA.address gives "Reference Error" for userA.address.zipcode.length, but optional chaining prevents it.
+
+
