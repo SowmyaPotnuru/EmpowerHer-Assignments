3940328ddfb4e23139c4e4d4e025739a003b216a
SowmyaPotnuru <sowmyapotnuru781@gmail.com>
2025-11-28 23:57:28 +0530

    Added Q13 from Day-14

---
+++ Module-2/Day-14/Q13-Asynchronous_Timer.js
@@ -0,0 +1,11 @@
+function timer(duration, callback){
+  setTimeout(() => {
+    callback(duration);
+  },duration);
+}
+
+function onComplete(duration){
+  console.log(`Timer of ${duration} ms finished`);
+}
+
+timer(5000,onComplete);
